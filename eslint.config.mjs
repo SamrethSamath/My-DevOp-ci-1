2dd30d93adb5026b8bbb880b3611371642701cb4
SamrethSamath <samathsmith070@gmail.com>
2026-03-11 16:18:40 +0700

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import globals from "globals";
+import jest from "eslint-plugin-jest";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["tests/**/*.test.js"], plugins: { jest }, extends: ["plugin:jest/recommended"], languageOptions: { globals: globals.jest } },
+]);
